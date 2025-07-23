d2bba8335ce853bfd64a6d85bea79d331b4d25bc
alexgrek <alexgrek.hq@gmail.com>
2025-07-24 02:23:15 +0300

    it renders!

--- src/game/render/light_sim/lighting.rs
+++ src/game/render/light_sim/lighting.rs
@@ -2,7 +2,7 @@ use bevy::{
     asset::RenderAssetUsages,
     color::palettes::css,
     prelude::*,
-    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat, TextureUsages},
     sprite::{Material2d, Material2dPlugin},
 };
 
@@ -23,7 +23,7 @@ use crate::{
 pub struct Lighting;
 
 pub const LIGHTING_OVERLAY_TILES: TilesCount = 32;
-pub const OVERLAY_IMAGE_SIZE: Units = LIGHTING_OVERLAY_TILES as isize * TILE_SIZE_IN_UNITS_UNITS;
+pub const OVERLAY_IMAGE_SIZE_SCALED: Units = LIGHTING_OVERLAY_TILES as isize * TILE_SIZE_IN_UNITS_UNITS;
 
 #[derive(Component)]
 pub struct OverlayImage(Handle<Image>);
@@ -38,73 +38,29 @@ impl Plugin for Lighting {
         // Register systems, resources, events, etc.
         app.add_systems(Update, overlay_texture_follow_camera);
         setup_directional_lights(app);
-        app.add_systems(Startup, setup_example);
+        app.add_systems(Startup, setup_overlay);
     }
 }
 
 fn setup_directional_lights(app: &mut App) {
     insert_chunked_plugin(app, LightsMapProducer::default(), 100);
-    app.add_systems(PostUpdate, simulation::run_fucking_simulation);
+    app.add_systems(PostUpdate, simulation::run_lights_simulation);
 }
 
 #[derive(Resource)]
 pub struct LightOverlayTextureHandle(pub Handle<Image>);
 
-fn setup_example(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
-    let IMAGE_WIDTH = 16;
-    let IMAGE_HEIGHT = 16;
-    let mut image = Image::new_fill(
-        // 2D image of size 256x256
-        Extent3d {
-            width: IMAGE_WIDTH,
-            height: IMAGE_HEIGHT,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        // Initialize it with a beige color
-        &(css::BEIGE.to_u8_array()),
-        // Use the same encoding as the color we set
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-    );
-
-    // To make it extra fancy, we can set the Alpha of each pixel,
-    // so that it fades out in a circular fashion.
-    for y in 0..IMAGE_HEIGHT {
-        for x in 0..IMAGE_WIDTH {
-            let center = Vec2::new(IMAGE_WIDTH as f32 / 2.0, IMAGE_HEIGHT as f32 / 2.0);
-            let max_radius = IMAGE_HEIGHT.min(IMAGE_WIDTH) as f32 / 2.0;
-            let r = Vec2::new(x as f32, y as f32).distance(center);
-            let a = 1.0 - (r / max_radius).clamp(0.0, 1.0);
-
-            // Here we will set the A value by accessing the raw data bytes.
-            // (it is the 4th byte of each pixel, as per our `TextureFormat`)
-
-            // Find our pixel by its coordinates
-            let pixel_bytes = image.pixel_bytes_mut(UVec3::new(x, y, 0)).unwrap();
-            // Convert our f32 to u8
-            pixel_bytes[3] = (a * u8::MAX as f32) as u8;
-        }
-    }
-
-    // Add it to Bevy's assets, so it can be used for rendering
-    // this will give us a handle we can use
-    // (to display it in a sprite, or as part of UI, etc.)
-    let handle = images.add(image);
-
-    // Create a sprite entity using our image
-    commands.spawn(Sprite::from_image(handle.clone()));
-    commands.insert_resource(LightOverlayTextureHandle(handle));
-}
+#[derive(Resource)]
+pub struct LightOverlayMaterialHandle(pub Handle<MultiplyBlendMaterial>);
 
 fn setup_overlay(
     mut commands: Commands,
     mut images: ResMut<Assets<Image>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut materials: ResMut<Assets<MultiplyBlendMaterial>>,
 ) {
     let color = css::AQUAMARINE.to_u8_array();
-    let size_unscaled = (OVERLAY_IMAGE_SIZE / TILE_SIZE_IN_UNITS_UNITS) as u32;
+    let size_unscaled = (OVERLAY_IMAGE_SIZE_SCALED / TILE_SIZE_IN_UNITS_UNITS) as u32;
     let image = Image::new_fill(
         // 2D image of size
         Extent3d {
@@ -120,22 +76,24 @@ fn setup_overlay(
         RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
     );
     let handle = images.add(image);
+
     // Additive Blend
-    let additive_material = ColorMaterial {
-        texture: Some(handle.clone()),
-        ..default()
+    let additive_material = MultiplyBlendMaterial {
+        texture: handle.clone(),
     };
     let mesh = meshes.add(Rectangle::new(
-        OVERLAY_IMAGE_SIZE as f32,
-        OVERLAY_IMAGE_SIZE as f32,
+        OVERLAY_IMAGE_SIZE_SCALED as f32,
+        OVERLAY_IMAGE_SIZE_SCALED as f32,
     ));
+    let material_handle = materials.add(additive_material);
     commands.spawn((
         OverlayImage(handle.clone()),
-        MeshMaterial2d(materials.add(additive_material)),
+        MeshMaterial2d(material_handle.clone()),
         Mesh2d(mesh),
         Transform::from_xyz(0.0, 0.0, 100000.0),
     ));
     commands.insert_resource(LightOverlayTextureHandle(handle));
+    commands.insert_resource(LightOverlayMaterialHandle(material_handle));
 }
 
 fn overlay_texture_follow_camera(
--- src/game/render/light_sim/simulation.rs
+++ src/game/render/light_sim/simulation.rs
@@ -8,13 +8,16 @@ use bevy::prelude::*;
 
 use crate::{
     core::{basics::Point, chunks::DataMap, constants::TILE_SIZE_IN_UNITS_UNITS},
-    game::render::light_sim::{
-        color_utils,
-        directions::Direction,
-        lighting::{
-            LIGHTING_OVERLAY_TILES, LightOverlayTextureHandle, OVERLAY_IMAGE_SIZE, OverlayImage,
+    game::render::{
+        blending::MultiplyBlendMaterial,
+        light_sim::{
+            color_utils,
+            directions::Direction,
+            lighting::{
+                LIGHTING_OVERLAY_TILES, LightOverlayMaterialHandle, LightOverlayTextureHandle, OverlayImage,
+            },
+            lights_map::LightsMapProducer,
         },
-        lights_map::LightsMapProducer,
     },
 };
 
@@ -60,94 +63,14 @@ impl Default for LightingBuffers {
     }
 }
 
-/// Every fixed update tick, draw one more pixel to make a spiral pattern
-pub(crate) fn draw(
-    my_handle: Res<LightOverlayTextureHandle>,
-    mut images: ResMut<Assets<Image>>,
-    // Used to keep track of where we are
-    mut i: Local<u32>,
-    mut draw_color: Local<Color>,
-) {
-    if *i == 0 {
-        // Generate a random color on first run.
-        let mut rng = rand::rng();
-        let color = Color::srgb(
-            rng.gen_range(0.0..=1.0),
-            rng.gen_range(0.0..=1.0),
-            rng.gen_range(0.0..=1.0),
-        );
-        *draw_color = color;
-    }
-
-    // Get the image from Bevy's asset storage.
-    let image = images.get_mut(&my_handle.0).expect("Image not found");
-
-    // Compute the position of the pixel to draw.
-
-    let center = Vec2::new(16 as f32 / 2.0, 16 as f32 / 2.0);
-    let max_radius = 16.min(16) as f32 / 2.0;
-    let rot_speed = 0.0123;
-    let period = 0.12345;
-
-    let r = ops::sin(*i as f32 * period) * max_radius;
-    let xy = Vec2::from_angle(*i as f32 * rot_speed) * r + center;
-    let (x, y) = (xy.x as u32, xy.y as u32);
-
-    // Get the old color of that pixel.
-    let old_color = image.get_color_at(x, y).unwrap();
-
-    // If the old color is our current color, change our drawing color.
-    let tolerance = 1.0 / 255.0;
-    if old_color.distance(&draw_color) <= tolerance {
-        let mut rng = rand::rng();
-        let color = Color::srgb(
-            rng.gen_range(0.0..=1.0),
-            rng.gen_range(0.0..=1.0),
-            rng.gen_range(0.0..=1.0),
-        );
-        *draw_color = color;
-    }
-
-    // Set the new color, but keep old alpha value from image.
-    image
-        .set_color_at(x, y, draw_color.with_alpha(old_color.alpha()))
-        .unwrap();
-
-    *i += 1;
-}
-
-/// Draws one random pixel with a random color at each update
-pub fn run_fucking_simulation(
-    light_texture_handle: Res<LightOverlayTextureHandle>,
-    mut images: ResMut<Assets<Image>>,
-) {
-    // Get mutable reference to the image
-    if let Some(image) = images.get_mut(&light_texture_handle.0) {
-        let width = image.size().x;
-        let height = image.size().y;
-
-        // Generate random coordinates and color
-        let mut rng = rand::rng();
-        let x = rng.gen_range(0..width);
-        let y = rng.gen_range(0..height);
-
-        let color = Color::srgb(
-            rng.gen_range(0.0..=1.0),
-            rng.gen_range(0.0..=1.0),
-            rng.gen_range(0.0..=1.0),
-        );
-
-        // Set the pixel
-        image.set_color_at(x, y, color).unwrap();
-    }
-}
-
 pub fn run_lights_simulation(
     light_texture_handle: Res<LightOverlayTextureHandle>,
     mut images: ResMut<Assets<Image>>,
     lightsources: Res<DataMap<LightsMapProducer>>,
     texture_world_position: Query<&Transform, With<OverlayImage>>,
     mut buffer: Local<LightingBuffers>,
+    light_material_handle: Res<LightOverlayMaterialHandle>,
+    mut materials: ResMut<Assets<MultiplyBlendMaterial>>,
 ) {
     // initialize buffers (if not yet initialized)
     if !buffer.initialized {
@@ -185,12 +108,12 @@ pub fn run_lights_simulation(
 
                 let center_tile_lightdata = center_tile_lightdata_cell.undirected_lights;
 
-                // if let Some(light) = center_tile_lightdata {
-                //     info!("Light detected: {x},{y}: {:?}", light.props.color);
-                //     for dir in Direction::ALL {
-                //         buffer.write[dir as usize][x][y] = light.props.color;
-                //     }
-                // }
+                if let Some(light) = center_tile_lightdata {
+                    // info!("Light detected: {x},{y}: {:?}", light.props.color);
+                    for dir in Direction::ALL {
+                        buffer.write[dir as usize][x][y] = light.props.color;
+                    }
+                }
             }
         }
         buffer.swap_buffers_clear_write();
@@ -204,13 +127,8 @@ pub fn run_lights_simulation(
         for x in 0..LIGHTING_OVERLAY_TILES {
             for y in 0..LIGHTING_OVERLAY_TILES {
                 let texture_y_px = y * TILE_SIZE_IN_UNITS_UNITS as usize;
-                let total_px = OVERLAY_IMAGE_SIZE as usize;
+                let total_px = LIGHTING_OVERLAY_TILES as usize;
                 if buffer.read[Direction::E as usize][x][y][0] > 0.0 {
-                    let my_x = x * TILE_SIZE_IN_UNITS_UNITS as usize;
-                    let my_y =
-                        total_px - (texture_y_px as isize + TILE_SIZE_IN_UNITS_UNITS) as usize;
-                    let srgba =
-                        Srgba::from_f32_array_no_alpha(buffer.read[Direction::E as usize][x][y]);
                     // info!(
                     //     "We detected something: {:?} at {my_x} {my_y}: {:?}; reference color: {:?}",
                     //     buffer.read[Direction::E as usize][x][y],
@@ -236,20 +154,14 @@ pub fn run_lights_simulation(
                 //     TILE_SIZE_IN_UNITS_UNITS as usize,
                 //     color_utils::convert_color(buffer.read[Direction::E as usize][x][y]),
                 // );
-                let color = if random_bool(0.5) {
-                    Color::from(Srgba::from_f32_array_no_alpha(
-                        buffer.read[Direction::E as usize][x][y],
-                    ))
-                } else {
-                    Color::from(css::LIMEGREEN)
-                };
-                image.set_color_at(x as u32, y as u32, color).unwrap();
+                let color = Color::from(Srgba::from_f32_array_no_alpha(
+                    buffer.read[Direction::E as usize][x][y],
+                ));
+                let x_coords = x;
+                let y_coords: isize = total_px as isize - (y as isize + 1);
+                image.set_color_at(x_coords as u32, y_coords as u32, color).unwrap();
             }
         }
     }
-    let image = images
-        .get(&light_texture_handle.0)
-        .expect("Image not found")
-        .clone();
-    images.insert(&light_texture_handle.0, image);
+    materials.get_mut(&light_material_handle.0);
 }
